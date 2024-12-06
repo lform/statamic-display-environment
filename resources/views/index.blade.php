0afc5f330543141f0e0e5eb025be9d4af9d7835b
Derek Skroch <derek@lform.com>
2024-12-06 14:14:16 -0600

    Updates letter spacing on display

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <div
-    class="px-2 py-1 mb-4 mt-2 text-xs text-center uppercase font-bold shadow-md rounded-md"
+    class="px-2 py-1 mb-4 mt-2 text-xs tracking-widest text-center uppercase font-bold shadow-md rounded-md"
     style="background-color: {{ $config["background"] }}; color: {{ $config["text"] }}"
 >
     {{ $env }}
